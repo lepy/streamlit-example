9182f60ea1da4e1fbdc842f0321042696b660889
ingolf.lepenies <ingolf.lepenies@scale.eu>
2020-11-13 19:29:46 +0100

    update phuzzy

--- streamlit_phuzzy.py
+++ streamlit_phuzzy.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 add_selectbox = st.sidebar.selectbox(
     "phuzzy numbers",
-    ("Triangle", "Uniform", "Trapezoid")
+    ("Triangle", "Uniform", "Trapezoid", "TruncNorm")
 )
 
 st.text('phuzzy')
@@ -21,12 +21,31 @@ mix_mpl(trapezoid)
 triangle = phuzzy.Triangle(alpha0=[1, 2],alpha1=[1.8])
 mix_mpl(triangle)
 
+truncnorm = phuzzy.TruncNorm(alpha0=[1, 2], alpha1=None, number_of_alpha_levels=7)
+mix_mpl(truncnorm)
+
+
 p_dict= {"Triangle":triangle,
          "Uniform":uniform,
-         "Trapezoid":trapezoid}
+         "Trapezoid":trapezoid,
+         "TruncNorm":truncnorm}
 print(add_selectbox)
 p = p_dict.get(add_selectbox, triangle)
 
+col1, col2 = st.sidebar.beta_columns(2)
+with col1:
+    p10 = st.number_input('a1.l', value=p.alpha1.l)
+    p00 = st.number_input('a0.l', value=p.alpha0.l, min_value=None, max_value=None)
+with col2:
+    p11 = st.number_input('a1.r', value=p.alpha1.r)
+    p01 = st.number_input('a0.r', value=p.alpha0.r)
+
+
+st.sidebar.write('Phuzzy', p)
+
+p.update(alpha0=[p00, p01], alpha1=[p10, p11])
+
+st.cache()
 def plot_phuzzy_number(p):
 
     H = 170.  #mm
@@ -39,3 +58,7 @@ def plot_phuzzy_number(p):
 plot_phuzzy_number(p)
 
 print(add_selectbox)
+
+
+
+
