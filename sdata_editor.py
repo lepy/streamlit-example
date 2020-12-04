362e891beff47d1ec0d24bb027411ab9049d9370
ingolf.lepenies <ingolf.lepenies@scale.eu>
2020-12-04 23:17:48 +0100

    add sdata example

--- sdata_editor.py
+++ sdata_editor.py
@@ -3,13 +3,16 @@ from streamlit_ace import st_ace
 import pandas as pd
 import numpy as np
 import sdata
+import uuid
 
 st.markdown("# sdata example")
+st.sidebar.markdown("## sdata")
+
 
 @st.cache(persist=False, allow_output_mutation=True)
 def get_sdata(name):
-    df = pd.DataFrame({"a":[1.1,2.1,3.5],
-                       "b":[2.4,1.2,2.2]})
+    df = pd.DataFrame({"a": [1.1, 2.1, 3.5],
+                       "b": [2.4, 1.2, 2.2]})
     d = sdata.Data(name="data", uuid="38b26864e7794f5182d38459bab85842", table=df)
     d.metadata.add("Temp", value=25.4, dtype="float", unit="degC", description="Temperatur")
     d.comment = """# header
@@ -22,15 +25,13 @@ a remarkable text
 $f(x) = \\frac{1}{2}\\sin(x)$
 
 
-[SCALE](https://www.scale.eu/de/unternehmen/ansprechpartner)
+[SCALE](https://www.scale.eu/)
 """
     return d
 
-
 # print(help(st_ace))
-
 # Spawn a new Ace editor
-#st_ace(value='', placeholder='', height=500, language='plain_text', theme='chrome',
+# st_ace(value='', placeholder='', height=500, language='plain_text', theme='chrome',
 # keybinding='vscode', min_lines=None, max_lines=None, font_size=12,
 # tab_size=4, wrap=False, show_gutter=True, show_print_margin=False,
 # readonly=False, annotations=None, markers=None, auto_update=True, key=None)
@@ -38,25 +39,28 @@ $f(x) = \\frac{1}{2}\\sin(x)$
 data = get_sdata("data")
 content_metadata = data.metadata.to_csv(sep=";", header=None)
 
+gen_uuid = st.sidebar.button("gen uuid")
+
+
 
 sdatapart = st.sidebar.radio(
     "choose sdata.Data component:",
     ('Metadata', 'Table', 'Comment'))
-if sdatapart == 'Metadata':
-    # ex = st.sidebar.button("create example data")
-    # if ex:
-    #     content_metadata = sample_content_metadata
-    # else:
-    #     content_metadata = ""
 
+
+
+if sdatapart == 'Metadata':
     st.markdown('## Metadata')
     # content_metadata = get_content("metadata")
+    content_metadata = data.metadata.to_csv(sep=";", header=None)
+
     content_metadata = content_metadata.replace("\t", ";")
 
     st.write("name; value; dtype; unit; description")
     content_metadata = st_ace(key="Meta", height=100, placeholder=content_metadata, value=content_metadata,
                               language="rst")
-    #st.write(content_metadata)
+    content_metadata = content_metadata.replace("\t", ";")
+    # st.write(content_metadata)
     cells = []
     for line in content_metadata.splitlines():
         cells.append(line.split(";"))
@@ -64,11 +68,12 @@ if sdatapart == 'Metadata':
         df = pd.DataFrame(cells, columns=['name', 'value', 'dtype', 'unit', 'description'])
         # st.write("parsed data")
         # st.dataframe(df)
-        data.metadata._attributes= {}
+        data.metadata._attributes = {}
         for i, row in df.iterrows():
             # print(row)
             # print(row["name"], row.value, str(row.dtype), row.unit, row.description)
-            data.metadata.add(row["name"], value=row["value"], dtype=str(row["dtype"]), unit=row["unit"], description=row["description"])
+            data.metadata.add(row["name"], value=row["value"], dtype=str(row["dtype"]), unit=row["unit"],
+                              description=row["description"])
 
     except Exception as exp:
         st.markdown("### error: {}".format(exp))
@@ -109,21 +114,24 @@ elif sdatapart == 'Comment':
 else:
     st.write("You didn't select anything.")
 
+if gen_uuid:
+    new_uuid = uuid.uuid4()
+    data.uuid = new_uuid
+
 st.sidebar.markdown("## sdata.Data Status")
+st.sidebar.markdown("{}".format(data.name))
+st.sidebar.markdown("{}".format(data.uuid))
 st.sidebar.dataframe(data.describe())
 
 ex = st.button("export data")
 if ex:
-
-
     st.markdown("## sdata.Data json")
     content_json = st_ace(key="json", height=100, value=data.to_json(), readonly=True,
                           language="json", wrap=True)
 
-
-    #data2 = data.from_json(data.to_json())
+    # data2 = data.from_json(data.to_json())
     st.markdown("## example python code")
-    content_python= r"""# python example
+    content_python = r"""# python example
 import sdata
 json_str = r'''{}'''
 data = sdata.Data.from_json(json_str)
@@ -132,6 +140,11 @@ print(data.metadata.df)
 print(data.df)
 print(data.comment)
 """.format(data.to_json())
-    content_json = st_ace(key="python", height=100, value=content_python, readonly=True,
+    content_json = st_ace(key="python", height=200, value=content_python, readonly=True,
                           language="python", wrap=True)
 
+st.sidebar.markdown("""* [SCALE](https://www.scale.eu/de/dienst/fem-methoden)
+* [sdata.git](https://github.com/lepy/sdata)
+* [sdata.doc](https://sdata.readthedocs.io/en/latest/index.html)
+
+""")
