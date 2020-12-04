b38257fb4c1f6ccd16c3feaedd551396a2408f93
ingolf.lepenies <ingolf.lepenies@scale.eu>
2020-12-04 22:40:14 +0100

    add sdata example

--- sdata_editor.py
+++ sdata_editor.py
@@ -8,21 +8,11 @@ st.markdown("# sdata example")
 
 @st.cache(persist=False, allow_output_mutation=True)
 def get_sdata(name):
-    return sdata.Data(name="data", uuid="38b26864e7794f5182d38459bab85842")
-
-
-# print(help(st_ace))
-
-# Spawn a new Ace editor
-#st_ace(value='', placeholder='', height=500, language='plain_text', theme='chrome',
-# keybinding='vscode', min_lines=None, max_lines=None, font_size=12,
-# tab_size=4, wrap=False, show_gutter=True, show_print_margin=False,
-# readonly=False, annotations=None, markers=None, auto_update=True, key=None)
-
-sample_content_metadata = """name	data	str	-	
-uuid	38b26864e7794f5182d38459bab85842	str	-	
-"""
-sample_content_comment="""# header
+    df = pd.DataFrame({"a":[1.1,2.1,3.5],
+                       "b":[2.4,1.2,2.2]})
+    d = sdata.Data(name="data", uuid="38b26864e7794f5182d38459bab85842", table=df)
+    d.metadata.add("Temp", value=25.4, dtype="float", unit="degC", description="Temperatur")
+    d.comment = """# header
 ## subheader
 
 a remarkable text
@@ -33,25 +23,21 @@ $f(x) = \\frac{1}{2}\\sin(x)$
 
 
 [SCALE](https://www.scale.eu/de/unternehmen/ansprechpartner)
-
 """
-sample_content_table = """name	value
-1	1.2
-2	2.3"""
+    return d
 
-@st.cache(persist=False, allow_output_mutation=True)
-def get_content(component):
-    print("get_content {}".format(component))
-    if component=="metadata":
-        return sample_content_metadata
-    elif component == "table":
-        return sample_content_table
-    elif component == "comment":
-        return sample_content_comment
-    else:
-        return None
+
+# print(help(st_ace))
+
+# Spawn a new Ace editor
+#st_ace(value='', placeholder='', height=500, language='plain_text', theme='chrome',
+# keybinding='vscode', min_lines=None, max_lines=None, font_size=12,
+# tab_size=4, wrap=False, show_gutter=True, show_print_margin=False,
+# readonly=False, annotations=None, markers=None, auto_update=True, key=None)
 
 data = get_sdata("data")
+content_metadata = data.metadata.to_csv(sep=";", header=None)
+
 
 sdatapart = st.sidebar.radio(
     "choose sdata.Data component:",
@@ -64,11 +50,11 @@ if sdatapart == 'Metadata':
     #     content_metadata = ""
 
     st.markdown('## Metadata')
-    content_metadata = get_content("metadata")
+    # content_metadata = get_content("metadata")
     content_metadata = content_metadata.replace("\t", ";")
 
     st.write("name; value; dtype; unit; description")
-    content_metadata = st_ace(key="Meta", height=100, placeholder=sample_content_metadata, value=content_metadata,
+    content_metadata = st_ace(key="Meta", height=100, placeholder=content_metadata, value=content_metadata,
                               language="rst")
     #st.write(content_metadata)
     cells = []
@@ -92,17 +78,18 @@ if sdatapart == 'Metadata':
 
 elif sdatapart == 'Table':
     st.markdown('## Table')
-    content_table = get_content("table")
+    # content_table = get_content("table")
+    content_table = data.df.to_csv(sep=";", index=None)
     content_table = content_table.replace("\t", ";")
-    content_table = st_ace(key="table", height=100, placeholder=sample_content_table, value=content_table)
+    content_table = st_ace(key="table", height=100, placeholder=content_table, value=content_table)
     content_table = content_table.replace("\t", ";")
     cells = []
     for line in content_table.splitlines():
         cells.append(line.split(";"))
     try:
         df = pd.DataFrame(cells[1:], columns=cells[0])
-        st.write("parsed data")
-        st.dataframe(df)
+        # st.write("parsed data")
+        # st.dataframe(df)
         data.df = df
     except:
         st.write(cells)
@@ -113,7 +100,7 @@ elif sdatapart == 'Table':
 
 elif sdatapart == 'Comment':
     st.markdown('## Comment')
-    content_comment = get_content("comment")
+    content_comment = data.comment
     content_comment = st_ace(key="comment", height=100, placeholder=content_comment,
                              language="markdown", value=content_comment)
     st.write(content_comment)
@@ -130,34 +117,21 @@ if ex:
 
 
     st.markdown("## sdata.Data json")
-    content_json = st_ace(key="json", height=100, placeholder=sample_content_table, value=data.to_json(), readonly=True,
+    content_json = st_ace(key="json", height=100, value=data.to_json(), readonly=True,
                           language="json", wrap=True)
 
 
     #data2 = data.from_json(data.to_json())
     st.markdown("## example python code")
     content_python= r"""# python example
-    import sdata
-    json_str = r'''{}'''
-    data = sdata.Data.from_json(json_str)
-    print(data.describe())
-    print(data.metadata.df)
-    print(data.df)
-    print(data.comment)
-    """.format(data.to_json())
-    content_json = st_ace(key="python", height=100, placeholder=sample_content_table, value=content_python, readonly=True,
+import sdata
+json_str = r'''{}'''
+data = sdata.Data.from_json(json_str)
+print(data.describe())
+print(data.metadata.df)
+print(data.df)
+print(data.comment)
+""".format(data.to_json())
+    content_json = st_ace(key="python", height=100, value=content_python, readonly=True,
                           language="python", wrap=True)
 
-
-
-    # Display editor's content as you type
-
-    # cells = []
-    # for line in content.splitlines():
-    #     cells.append(line.split("\t"))
-    # df = pd.DataFrame(cells[1:], columns=cells[0])
-    #
-    # st.dataframe(df)
-    #
-    # st.write(content)
-    # # for lin in lines
